e8efde6c64008afaacca62167b291fa7014cfe05
yrajm1997 <yograj.m@talentsprint.com>
2025-03-21 19:48:39 +0530

    solution files removed

--- src/main/java/com/mystore/app/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mystore.app.repositories;
-
-import com.mystore.app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    public User findByUsername(String username);
-}
